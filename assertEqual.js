15ec3bb2333d466468eae386a3e89a337a015bdb
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-21 04:14:01 +0000

    Creating an Assert function which return a message with emojis

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F600) + String.fromCodePoint(0x1F600) + `Assertion Passed:${actual} === ${expected} `);
+
+  } else {
+    console.log(String.fromCodePoint(0x1F6D1) + String.fromCodePoint(0x1F6D1) + `Assertion Failed:${actual} !== ${expected} `);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("lighthouse labs", "Lighthouse Labs");
+assertEqual(1, 1);
