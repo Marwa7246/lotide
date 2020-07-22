16a54e48fff5929f7849ea10309375e2a841dcfb
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-22 18:59:40 +0000

    Refracted the function by breaking it down  into 2 functions, one for comparison and the other for printing.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,25 @@
-const assertArraysEqual = function(array1, array2) {
-  
+const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      console.log(String.fromCodePoint(0x1F6D1) + String.fromCodePoint(0x1F6D1) + "Assertion Failed:" , array1 , '!==' , array2);
       return false;
     }
   }
-  console.log(String.fromCodePoint(0x1F600) + String.fromCodePoint(0x1F600) + "Assertion Passed:" , array1 , '===' , array2);
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  const result = eqArrays(array1, array2);
+  
+  if (result) {
+    console.log(String.fromCodePoint(0x1F600) , String.fromCodePoint(0x1F600) , 'Assertion Passed:' , array1 , '===' , array2);
+
+  } else {
+    console.log(String.fromCodePoint(0x1F6D1) , String.fromCodePoint(0x1F6D1) , 'Assertion Failed:' , array1 , '!==' , array2);
+  }
+
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([4, 2, 3], [3, 2, 1]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", 5]);
-
-
