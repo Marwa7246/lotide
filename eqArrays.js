4d93c213e0d807063f5c1d55cb3de38cdcb7e9cf
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-23 16:41:19 +0000

    changed the last boolean in asserEqual from true to false

--- eqArrays.js
+++ eqArrays.js
@@ -18,14 +18,14 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays(1, 1)); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(1, 1), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
