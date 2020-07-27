4e2db61dd1d3aaf11ca812e1455b1da892b658f3
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-27 19:01:39 +0000

    Refactoring eqArrays by moving the test code to anew file eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(String.fromCodePoint(0x1F600) + String.fromCodePoint(0x1F600) + `Assertion Passed:${actual} === ${expected} `);
-
-  } else {
-    console.log(String.fromCodePoint(0x1F6D1) + String.fromCodePoint(0x1F6D1) + `Assertion Failed:${actual} !== ${expected} `);
-  }
-
-};
-
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -18,14 +8,10 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(eqArrays(1, 1)); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+//console.log(eqArrays(1, 1)); // => true
+//console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
 
-// TEST CODE
-assertEqual(eqArrays(1, 1), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
