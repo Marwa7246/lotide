e1f8822da91068f9cbf07efd08f0c26927a834d5
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-07-27 20:11:35 +0000

    Refactoring middle.js by moving the test code to a new file middleTest, exporting the module middle and requiring assertArraysEqual and middle from within the new file

--- middle.js
+++ middle.js
@@ -1,27 +1,7 @@
-//compare 2 arrays and return true if they are equal
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require ('./eqArrays');
+const assertArraysEqual = require ('./assertArraysEqual');
 
-// prient a message is the result of the comparison if the sam as was expected
-const assertArraysEqual = function(array1, array2) {
-  const result = eqArrays(array1, array2);
-  
-  if (result) {
-    console.log(String.fromCodePoint(0x1F600) , String.fromCodePoint(0x1F600) , 'Assertion Passed:' , array1 , '===' , array2);
-
-  } else {
-    console.log(String.fromCodePoint(0x1F6D1) , String.fromCodePoint(0x1F6D1) , 'Assertion Failed:' , array1 , '!==' , array2);
-  }
-
-};
-
-//A function that return the middle element if an array
+//A function that return the middle element of an array
 const middle = function(array) {
   // if number of elements is less than 2, return an empty array
   let result = [];
@@ -37,8 +17,5 @@ const middle = function(array) {
   return (result);
 };
 
-//console.log(middle([1, 2, 3, 4, 5]));
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1]), []);
+module.exports = (middle);
+
